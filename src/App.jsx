d876fac228fdba7215effdb8db24854a16808540
Raushan Kumar Thakur <raushankumarthakur311@gmail.com>
2024-08-09 01:28:27 +0530

    Fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
               type="checkbox"
               checked={charAllowed}
               id="characterInput"
-              onChange={() => {cd 
+              onChange={() => {
                 setCharAllowed((prev) => !prev);
               }}
             />
